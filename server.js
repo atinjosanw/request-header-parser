59607be7fb6a60660611458e86f65543ab038d47
Yarting <atinjosan@yahoo.com>
2016-01-12 21:17:42 +0000

    Finish ipaddress, language and software field retrieval.

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 var express = require('express'),
+    accepts = require('accepts'),
     app = express(), 
     router = express.Router(),
     port = process.env.PORT || 3000;
     
 router.use(function (req, res, next) {
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  res.send(ip);
-  console.log("received request from " + ip);
+  var languages = accepts(req).languages();
+  if (languages.length == 1)
+    languages = languages[0];
+  var userAgent = req.headers['user-agent'].match(/^([\w\/\.]+) \(([\w\s\.]+; [\w\s]+)\)( .*)/), op = "";
+  if (userAgent) {
+    op = userAgent[2];
+  }
+  var str = {"ipaddress":ip, "language":languages, "software":op};
+  res.json(str);
+  console.log("Receiving request from " + ip + "\n");
 });
 
 app.use('*', router);
 
-// app.enable('trust proxy');
-
-var server = app.listen(port, function () {
+app.listen(port, function () {
   console.log("The node server is running.");
 });
     
